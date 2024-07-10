2188c385b0be73997b18dbfc3e7a53501b666dd2
MarcellusStack <MarcellusStack@gmail.com>
2024-07-10 17:06:20 +0200

    setup auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,41 @@
 import { AuthLayout } from "@components/layouts/auth-layout";
 import { Button, Stack, Text, Title } from "@mantine/core";
+import { auth, signIn, signOut } from "@server/auth";
 import { IconBrandGoogle } from "@tabler/icons-react";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <AuthLayout>
       <Stack gap="sm">
         <Title order={1}>Login</Title>
         <Text>Welcome! Please select a provider to log in.</Text>
         {process.env.NEXT_PUBLIC_GOOGLE_LOGIN === "enable" && (
-          <Button
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <button type="submit">Signin with Google</button>
+          </form>
+        )}
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">Sign out</button>
+        </form>
+      </Stack>
+    </AuthLayout>
+  );
+}
+
+{
+  /* <Button
             variant="light"
             leftSection={
               <IconBrandGoogle style={{ width: "70%", height: "70%" }} />
@@ -17,9 +43,5 @@ export default function Home() {
             color="red"
           >
             Continue with Google
-          </Button>
-        )}
-      </Stack>
-    </AuthLayout>
-  );
+          </Button> */
 }
