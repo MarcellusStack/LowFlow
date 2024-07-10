2ceeb5b2f2c880074016433ccfd7c3d1669ce1a6
MarcellusStack <MarcellusStack@gmail.com>
2024-07-10 16:08:52 +0200

    added login page and branding

---
+++ components/branding.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { Box, Text } from "@mantine/core";
+
+export const Branding = () => {
+  return (
+    <Link
+      href="/"
+      passHref
+      className="flex items-center gap-3 text-black"
+      style={{ textDecoration: "none" }}
+    >
+      <Box className="p-3 rounded-sm bg-gray-100 grid place-items-center">
+        <Image src="/branding.png" width={32} height={32} alt="branding" />
+      </Box>
+      <Text fw={700} size="xl">
+        LowFlow
+      </Text>
+    </Link>
+  );
+};
---
+++ components/layouts/auth-layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Branding } from "@components/branding";
+import { Box, Center, Flex, Stack } from "@mantine/core";
+import React from "react";
+
+export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box h="100vh">
+      <Flex h="100%" w="100%">
+        <Stack
+          className="w-full md:w-1/2"
+          p="md"
+          justify="space-between"
+          align="center"
+        >
+          <Flex justify="space-between" className="w-full">
+            <Branding />
+          </Flex>
+          <Box h="100%" w="50%" bg="white">
+            <Center h="100%">{children}</Center>
+          </Box>
+          <Box />
+        </Stack>
+        <Box h="100%" w="50%" bg="black"></Box>
+      </Flex>
+    </Box>
+  );
+};
