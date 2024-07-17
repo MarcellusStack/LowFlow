a9db8034a78fa78437608f69e1919722bea9640a
MarcellusStack <MarcellusStack@gmail.com>
2024-07-17 13:37:50 +0200

    added app layout and nav links

---
+++ components/layouts/app-layout.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { Branding } from "@components/branding";
+import { UserButton } from "@components/user-button";
+import { AppShell, Button, Flex, Loader } from "@mantine/core";
+import { IconArrowsRandom, IconLayoutDashboard } from "@tabler/icons-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useTransition } from "react";
+
+export type NavLinkProps = {
+  id: string;
+  href: string;
+  icon: React.ReactNode;
+  label: string;
+  disabled?: boolean;
+};
+
+export const navLinks: NavLinkProps[] = [
+  {
+    id: "nav-GD5KD7GNBB",
+    href: "/dashboard",
+    icon: <IconLayoutDashboard stroke={1.5} />,
+    label: "Dashboard",
+  },
+  {
+    id: "nav-2TCR0AX9Z2",
+    href: "/workflows",
+    icon: <IconArrowsRandom stroke={1.5} />,
+    label: "Workflows",
+  },
+];
+
+const NavLink = ({ link }: { link: NavLinkProps }) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  return (
+    <Button
+      color="black"
+      justify="flex-start"
+      variant="subtle"
+      component={Link}
+      href={link.href}
+      onClick={() => startTransition(() => router.push(link.href))}
+      disabled={link.disabled && link.disabled}
+      leftSection={isPending ? <Loader size="xs" /> : link.icon}
+    >
+      {link.label}
+    </Button>
+  );
+};
+
+export const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: false } }}
+      padding="sm"
+    >
+      <AppShell.Header px="sm">
+        <Flex justify="space-between" align="center" className="w-full h-full">
+          <Branding />
+          <UserButton />
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Navbar p="sm">
+        {navLinks.map((link) => (
+          <NavLink link={link} key={link.id} />
+        ))}
+      </AppShell.Navbar>
+      <AppShell.Main p="sm">{children}</AppShell.Main>
+    </AppShell>
+  );
+};
