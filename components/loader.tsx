b483c4a285b0f151c9dcedbc668cfd84de610088
MarcellusStack <MarcellusStack@gmail.com>
2024-07-18 11:22:55 +0200

    added loader component

---
+++ components/loader.tsx
@@ -0,0 +1,15 @@
+import { Flex, Loader as Spinner } from "@mantine/core";
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      direction="column"
+      style={{ height: "calc(100vh - 137px)" }}
+    >
+      <Spinner color="blue" />
+    </Flex>
+  );
+};
