6a80027bb69bc7bf3f9ff46fdd9334a6444b15fe
MarcellusStack <MarcellusStack@gmail.com>
2024-08-12 11:47:01 +0200

    clean up

--- app/(app)/runs/_components/runs.tsx
+++ app/(app)/runs/_components/runs.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { getRuns } from "../_actions";
 import { RunsTable } from "./runs-table";
-import { RunDataTable } from "./run-data-table";
-import { TestDataTable } from "./run-data-table";
 
 export const Runs = async ({
   searchParams,
